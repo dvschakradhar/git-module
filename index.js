e43d17f444c04404a30c6638c81e0e9ae888a4ec
dvschakradhar <chakradhar.dittakavi@gmail.com>
2023-07-26 20:19:22 +0530

    fix: addressed tarun's comment

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 console.log("Hello world!");
 console.log("10x is awesome");
 console.log("Chakri is awesome");
-console.log("Another comment");
-consolelog("Another comment for amit-ticket-1");
+console.log("Another comment for amit-ticket-1");
